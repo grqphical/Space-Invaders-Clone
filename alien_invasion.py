0be671337d0e536f102334d1bbcf6f719ba986aa
grqphical <thundercobra48@gmail.com>
2022-01-10 19:31:05 -0800

    Added the ability to create alien fleets

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,8 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -12,14 +14,23 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Space Invaders Clone")
+        pygame.display.set_icon(pygame.image.load('images/ship.png'))
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self.create_fleet()
 
         self.bg_colour = self.settings.bg_colour
     def run_game(self):
         while True:
             self.check_events()
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
             self.update_screen()
+            self.bullets.update()
             self.ship.update()
 
     def check_events(self):
@@ -38,11 +49,42 @@ class AlienInvasion:
                         self.ship.moving_right = False
                     elif event.key == pygame.K_a:
                         self.ship.moving_left = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_presses = pygame.mouse.get_pressed()
+                    if mouse_presses[0]:
+                        self._fire_bullet()
     def update_screen(self):
             self.screen.fill(self.bg_colour)
             self.ship.blitme()
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
+            self.aliens.draw(self.screen)
             pygame.display.flip()
+    
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet) 
+    def create_fleet(self):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        avalible_space_x = self.settings.screen_width - (2 * alien_width)
+        number_of_aliens_x = avalible_space_x // (2 * alien_width)
 
+        ship_height = self.ship.rect.height
+        avalible_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+
+        number_rows = avalible_space_y // (2 * alien_height)
+        for row in range(number_rows):
+            for alien_number in range(number_of_aliens_x):
+                self.create_alien(alien_number, row)
+    def create_alien(self, alien_number, row):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row
+        self.aliens.add(alien)
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
--- settings.py
+++ settings.py
@@ -3,7 +3,14 @@ class Settings:
         # Screen Settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_colour = (90,90,90)
+        self.bg_colour = (50,50,50)
 
         # Ship Settings
         self.ship_speed = 1.5
+
+        # Bullet Settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = (124, 252, 0)
+        self.bullets_allowed = 3
