eb6e98309db7c478356bb0ba42304dddb0d16eaf
grqphical <thundercobra48@gmail.com>
2022-01-10 16:01:12 -0800

    Added Settings Class and updated alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,18 @@
 import sys
 
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Space Invaders Clone")
 
-        self.bg_colour = (80,80,80)
+        self.bg_colour = self.settings.bg_colour
     
     def run_game(self):
         while True:
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    def __init__(self):
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (90,90,90)
