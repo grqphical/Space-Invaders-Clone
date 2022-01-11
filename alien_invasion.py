70efa805e0bc1c6d7e52fbcde8938d9d8237e054
grqphical <thundercobra48@gmail.com>
2022-01-10 16:17:57 -0800

    added Ship class to draw the ship to the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -12,6 +13,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Space Invaders Clone")
 
+        self.ship = Ship(self)
+
         self.bg_colour = self.settings.bg_colour
     
     def run_game(self):
@@ -21,6 +24,7 @@ class AlienInvasion:
                     sys.exit()
 
             self.screen.fill(self.bg_colour)
+            self.ship.blitme()
             pygame.display.flip()
 
 if __name__ == '__main__':
